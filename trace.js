b6afea81ad425d1f73e5d803e55ef2719e0389df
buggyj <buggyjef@gmail.com>
2014-02-15 11:52:03 +0530

    initial

--- trace.js
+++ trace.js
@@ -2,25 +2,16 @@
 title: $:/plugins/bj/Dev/trace.js
 type: application/javascript
 module-type: wikimethod
-Display stacktrace
+Display stacktrace if supported by browser
 \*/
-if($tw.browser) {  // alert("hilow");
-
-$tw.utils.error2 = function (message, file, line, column, errorObj)
-{
-    var errobject;
-    //if (!!errorObj &&!!errorObj.stack)  {alert("myerror"+errorObj.stack);}//throw(errorObj);}
-    //else 
-    $tw.utils.error(message, file, line, column, errorObj);
-    
-   //var trace = printStackTrace({e:errorObj});
-//if (errobject ==true)
-  // alert(errorObj+trace.join('\n\n'));
-   //else
-   //alert(trace.join('\n\n')); 
-   //$tw.utils.errorOriginal(err);
-   //alert("hi");
-    
-}
-window.onerror = $tw.utils.error2;
+if($tw.browser) {  
+	$tw.utils.error2 = function (message, file, line, column, errorObj)
+	{
+		if (!!errorObj &&!!errorObj.stack) {
+			alert(errorObj.stack);
+		} else {
+			$tw.utils.error(message, file, line);
+		}    
+	}
+	window.onerror = $tw.utils.error2;
 }
